b028ba15de7fcc3eff6508ce20ecdd0ccbf8a947
yuvarjkarna2717 <173258726+yuvrajkarna2717@users.noreply.github.com>
2025-01-03 20:54:17 +0530

    refactor and added multiple scrape functions

--- utils/scrapper.js
+++ utils/scrapper.js
@@ -2,8 +2,61 @@ import puppeteer from "puppeteer";
 import dotenv from "dotenv";
 dotenv.config();
 
-async function scrape() {
+const scrapeCoreLogic = async (page, pageNumber) => {
   const WEBSITE_URL = process.env.WEBSITE_URL;
+  const url = `${WEBSITE_URL}/catalogue/page-${pageNumber}.html`;
+  await page.goto(url);
+
+  const booksOnPage = await page.evaluate(() => {
+    const bookElements = document.querySelectorAll(".product_pod");
+
+    return Array.from(bookElements).map((book) => {
+      const title = book.querySelector("h3 a").getAttribute("title");
+      const price = book.querySelector(".price_color").textContent;
+      const stock = book.querySelector(".instock.availability")
+        ? "In Stock"
+        : "Out Of Stock";
+      const rating = book.querySelector(".star-rating").className.split(" ")[1];
+      const link =
+        `https://books.toscrape.com/catalogue/` +
+        book.querySelector("h3 a").getAttribute("href");
+
+      return {
+        title,
+        price,
+        stock,
+        rating,
+        link,
+      };
+    });
+  });
+
+  for (let book of booksOnPage) {
+    await page.goto(book.link);
+    const stockInfo = await page.evaluate(() => {
+      const stockInfoElement = document.querySelector(".instock.availability");
+      return stockInfoElement
+        ? stockInfoElement.innerText.trim()
+        : "Unknown Stock Info";
+    });
+    book.stockInfo = stockInfo;
+
+    const imageLink = await page.evaluate(() => {
+      const imageLinkElement = document.querySelector(".item.active");
+
+      // Find the img tag within the selected element
+      const imgElement = imageLinkElement.querySelector("img");
+
+      // Get the src attribute of the img tag
+      return imgElement ? imgElement.src : null;
+    });
+    book.imageLink = imageLink;
+  }
+
+  return booksOnPage;
+};
+
+async function scrape() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -12,59 +65,7 @@ async function scrape() {
     totalPageNumber = 50;
 
   while (pageNumber <= totalPageNumber) {
-    const url = `${WEBSITE_URL}/catalogue/page-${pageNumber}.html`;
-    await page.goto(url);
-
-    const booksOnPage = await page.evaluate(() => {
-      const bookElements = document.querySelectorAll(".product_pod");
-
-      return Array.from(bookElements).map((book) => {
-        const title = book.querySelector("h3 a").getAttribute("title");
-        const price = book.querySelector(".price_color").textContent;
-        const stock = book.querySelector(".instock.availability")
-          ? "In Stock"
-          : "Out Of Stock";
-        const rating = book
-          .querySelector(".star-rating")
-          .className.split(" ")[1];
-        const link =
-          `https://books.toscrape.com/catalogue/` +
-          book.querySelector("h3 a").getAttribute("href");
-
-        return {
-          title,
-          price,
-          stock,
-          rating,
-          link,
-        };
-      });
-    });
-
-    for (let book of booksOnPage) {
-      await page.goto(book.link);
-      const stockInfo = await page.evaluate(() => {
-        const stockInfoElement = document.querySelector(
-          ".instock.availability"
-        );
-        return stockInfoElement
-          ? stockInfoElement.innerText.trim()
-          : "Unknown Stock Info";
-      });
-      book.stockInfo = stockInfo;
-
-      const imageLink = await page.evaluate(() => {
-        const imageLinkElement = document.querySelector(".item.active");
-
-        // Find the img tag within the selected element
-        const imgElement = imageLinkElement.querySelector("img");
-
-        // Get the src attribute of the img tag
-        return imgElement ? imgElement.src : null;
-      });
-      book.imageLink = imageLink;
-    }
-
+    const booksOnPage = await scrapeCoreLogic(page, pageNumber);
     allBooks.push(...booksOnPage);
     pageNumber++;
   }
@@ -78,141 +79,18 @@ async function scrapeSinglePage(pageNumber = 1) {
 }
 
 async function scrapeFromPageXToPageY(pageX = 1, pageY = 2) {
-  const WEBSITE_URL = process.env.WEBSITE_URL;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   let allBookDetails = [];
 
   for (let bookPage = pageX; bookPage <= pageY; bookPage++) {
-    const url = `${WEBSITE_URL}/catalogue/page-${bookPage}.html`;
-
-    await page.goto(url);
-    const booksOnPage = await page.evaluate(() => {
-      const bookElements = document.querySelectorAll(".product_pod");
-
-      return Array.from(bookElements).map((book) => {
-        const title = book.querySelector("h3 a").getAttribute("title");
-        const price = book.querySelector(".price_color").textContent;
-        const stock = book.querySelector(".instock.availability")
-          ? "In Stock"
-          : "Out Of Stock";
-        const rating = book
-          .querySelector(".star-rating")
-          .className.split(" ")[1];
-        const link =
-          `https://books.toscrape.com/catalogue/` +
-          book.querySelector("h3 a").getAttribute("href");
-
-        return {
-          title,
-          price,
-          stock,
-          rating,
-          link,
-        };
-      });
-    });
-
-    for (let book of booksOnPage) {
-      await page.goto(book.link);
-      const stockInfo = await page.evaluate(() => {
-        const stockInfoElement = document.querySelector(
-          ".instock.availability"
-        );
-        return stockInfoElement
-          ? stockInfoElement.innerText.trim()
-          : "Unknown Stock Info";
-      });
-      book.stockInfo = stockInfo;
-
-      const imageLink = await page.evaluate(() => {
-        const imageLinkElement = document.querySelector(".item.active");
-
-        // Find the img tag within the selected element
-        const imgElement = imageLinkElement.querySelector("img");
-
-        // Get the src attribute of the img tag
-        return imgElement ? imgElement.src : null;
-      });
-      book.imageLink = imageLink;
-    }
+    const booksOnPage = await scrapeCoreLogic(page, bookPage);
     allBookDetails.push(...booksOnPage);
   }
 
   await browser.close();
   return allBookDetails;
 }
-
-// async function scrapeNBookDetails(n) {
-//   const WEBSITE_URL = process.env.WEBSITE_URL;
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-
-//   let allBookDetails = [];
-//   let bookPage = 1;
-
-//   while (allBookDetails.length <= n) {
-//     const url = `${WEBSITE_URL}/catalogue/page-${bookPage}.html`;
-
-//     await page.goto(url);
-//     const booksOnPage = await page.evaluate(() => {
-//       const bookElements = document.querySelectorAll(".product_pod");
-
-//       return Array.from(bookElements).map((book) => {
-//         const title = book.querySelector("h3 a").getAttribute("title");
-//         const price = book.querySelector(".price_color").textContent;
-//         const stock = book.querySelector(".instock.availability")
-//           ? "In Stock"
-//           : "Out Of Stock";
-//         const rating = book
-//           .querySelector(".star-rating")
-//           .className.split(" ")[1];
-//         const link =
-//           `https://books.toscrape.com/catalogue/` +
-//           book.querySelector("h3 a").getAttribute("href");
-
-//         return {
-//           title,
-//           price,
-//           stock,
-//           rating,
-//           link,
-//         };
-//       });
-//     });
-
-//     for (let book of booksOnPage) {
-//       await page.goto(book.link);
-//       const stockInfo = await page.evaluate(() => {
-//         const stockInfoElement = document.querySelector(
-//           ".instock.availability"
-//         );
-//         return stockInfoElement
-//           ? stockInfoElement.innerText.trim()
-//           : "Unknown Stock Info";
-//       });
-//       book.stockInfo = stockInfo;
-
-//       const imageLink = await page.evaluate(() => {
-//         const imageLinkElement = document.querySelector(".item.active");
-
-//         // Find the img tag within the selected element
-//         const imgElement = imageLinkElement.querySelector("img");
-
-//         // Get the src attribute of the img tag
-//         return imgElement ? imgElement.src : null;
-//       });
-//       book.imageLink = imageLink;
-//     }
-
-//     allBookDetails.push(...booksOnPage);
-//     if (allBookDetails.length <= n) break;
-//     bookPage++;
-//   }
-
-//   await browser.close();
-//   return allBookDetails;
-// }
 
 export { scrape, scrapeSinglePage, scrapeFromPageXToPageY };
