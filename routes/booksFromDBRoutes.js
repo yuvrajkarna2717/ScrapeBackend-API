ac4960cc2d4eb3fccccaf358a20e16d52cea54ab
yuvarjkarna2717 <173258726+yuvrajkarna2717@users.noreply.github.com>
2025-01-04 22:36:31 +0530

    added api's purpose comments

--- routes/booksFromDBRoutes.js
+++ routes/booksFromDBRoutes.js
@@ -11,11 +11,22 @@ import {
 
 const fetchBookFromDBController = express.Router();
 
+// to get all books
 fetchBookFromDBController.get("/all-books", getAllBooks);
+
+// get books by title
 fetchBookFromDBController.get("/books/title", getBooksByTitle);
+
+//get books by price range
 fetchBookFromDBController.get("/books/price-range", getBooksByPriceRange);
+
+//get books by rating range
 fetchBookFromDBController.get("/books/rating-range", getBooksByRatingsRange);
+
+//get n books bt some limit i.e no. of books
 fetchBookFromDBController.get("/books/limit", getBooksByLimit);
+
+// get books by sorting in price and rating in ascending or descending order
 fetchBookFromDBController.get("/books/sort", getBooksBySorting);
 
 export { fetchBookFromDBController };
